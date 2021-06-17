4e2105425000fe5041192c66b099208aa95e32fc
abhynadar <vabhynadar@yahoo.com>
2021-06-17 11:40:09 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: "singpraise",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        backend: {
+          name: "git-gateway",
+          branch: "master",
+        },
+        media_folder: "static/img",
+        public_folder: "/img"
+      }
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
