8ecf9079034bd0d3f360e76feeb7b77bca6c4d0a
abhynadar <vabhynadar@yahoo.com>
2021-06-20 20:29:45 +0530

    changes to collection for admin cms

---
+++ public/admin/cms.eb37d69aeb7301d3e87c.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdatesingpraise"]("cms",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "c6b6e0671322a07385d0"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.eb37d69aeb7301d3e87c.hot-update.js.map
