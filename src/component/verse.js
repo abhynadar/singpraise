8a7a61297f4b19cfdf151122558c347e61a0d553
abhynadar <vabhynadar@yahoo.com>
2021-07-04 20:09:38 +0530

    added verses

--- src/component/verse.js
+++ src/component/verse.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Col, Card, Row } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
 
 const Verse = ({ verse, index }) => {
     // console.log(verse);
     const { node: { frontmatter: verseData } } = verse;
-    // console.log(verseData);
+    console.log(verseData);
     const color = (index % 2 === 0) ? "light" : "info";
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Title>{verseData.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{verseData.tags}</Card.Subtitle>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    {verseData.content}
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
         </Card>
 
--- src/component/verseList.js
+++ src/component/verseList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Verse from './verse';
-import { Col, Container, Row } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 
 const VerseList = ({ data }) => {
     console.log('VerseList', data);
